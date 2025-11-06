07923a46d7c650705c44d9049b1e693eb7df1f61
SuvasD <suvasdandu@gmail.com>
2025-11-06 15:58:06 -0800

    Add files via upload

---
+++ tuple_ops.py
@@ -0,0 +1,12 @@
+tuplex=('tuple', 5, True, 3.2)
+tuplex=(2,3,4,5,7,0)
+print(tuplex)
+tuplex=tuplex+(9,)
+print(tuplex)
+tuple1=(50,10,60,70,50)
+print(tuple1.count(50))
+tuplex=(2,4,3,5,6,3,9,0,2)
+_slice=tuplex[3:5]
+print(_slice)
+_slice=tuplex[:6]
+print(_slice)
